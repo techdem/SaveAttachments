c02da9f01fc584ec3c20420e6194d0647ac9e300
techdem <tudor.chiribes@gmail.com>
2019-01-10 14:15:33 +0000

    Add project files.

---
+++ SaveAttachments/RibbonMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Outlook;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SaveAttachments
+{
+    public partial class RibbonMain
+    {
+        private void RibbonMain_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            string myURL = "http://documents.i.opw.ie";
+            System.Diagnostics.Process.Start(myURL);
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            var context = e.Control.Context as Inspector;
+
+            try
+            {
+                var mailItem = context.CurrentItem as MailItem;
+
+                if (mailItem != null)
+                {
+                    if (mailItem.Attachments.Count > 0)
+                    {
+                        FolderBrowserDialog pathDialog = new FolderBrowserDialog();
+
+                        if (pathDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            foreach (Attachment item in mailItem.Attachments)
+                            {
+                                item.SaveAsFile(Path.Combine(pathDialog.SelectedPath, item.FileName));
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid Path...");
+                        }
+
+                        MessageBox.Show("Attachments downloaded to folder!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No attachments found!");
+                    }
+                }
+            }
+
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Please select an e-mail first...\n\nException:\n " + ex);
+            }
+        }
+    }
+}
---
+++ SaveAttachments/RibbonMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Outlook;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SaveAttachments
+{
+    public partial class RibbonMessage
+    {
+        private void RibbonMessage_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            var context = e.Control.Context as Inspector;
+            var mailItem = context.CurrentItem as MailItem;
+
+            if (mailItem != null)
+            {
+                if (mailItem.Attachments.Count > 0)
+                {
+                    FolderBrowserDialog pathDialog = new FolderBrowserDialog();
+
+                    if (pathDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        foreach (Attachment item in mailItem.Attachments)
+                        {
+                            item.SaveAsFile(Path.Combine(pathDialog.SelectedPath, item.FileName));
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Path...");
+                    }
+
+                    MessageBox.Show("Attachments downloaded to folder!");
+                }
+                else
+                {
+                    MessageBox.Show("No attachments found!");
+                }
+            }
+        }
+    }
+}
