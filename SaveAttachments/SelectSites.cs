e6ed9f3b93abdb52ee55d7454d1afd60a90df8ba
techdem <tudor.chiribes@gmail.com>
2019-01-23 11:41:01 +0000

    added browsing through folders

--- SaveAttachments/SelectSites.cs
+++ SaveAttachments/SelectSites.cs
@@ -15,15 +15,15 @@ namespace SaveAttachments
 {
     public partial class SelectSites : Form
     {
-        string selectedAction;
-        dynamic sitesArray;
-        dynamic browseContent;
-
         ArrayList shortNames = new ArrayList();
         ArrayList nodeIDs = new ArrayList();
 
         WebClient client = new WebClient();
         JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+        string selectedAction;
+        dynamic sitesArray;
+        dynamic browseContent;
 
         public SelectSites (string action)
         {
@@ -43,33 +43,57 @@ namespace SaveAttachments
             foreach (dynamic d in sitesArray)
             {
                 listBox1.Items.Add(d["title"]);
-                shortNames.Add(d["shortName"]);
-                nodeIDs.Add(d["node"].Substring(d["node"].IndexOf("Store/") + 6));
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             String url;
+            String id = "";
 
-            if (listBox1.SelectedIndex == 0)
+            if (listBox1.GetItemText(listBox1.SelectedItem).Equals("Home"))
             {
-                listBox1.Items.Clear();
                 url = "https://documents.i.opw.ie/share/page/user/chiribest/dashboard";
-                browseContent = serializer.DeserializeObject(client.DownloadString("https://documents.i.opw.ie/alfresco/api/-default-/public/alfresco/versions/1/nodes/-my-/children"));
-
-                foreach (dynamic d in browseContent["list"]["entries"])
-                {
-                    listBox1.Items.Add(d["entry"]["name"]);
-                }
+                browseContent = serializer.DeserializeObject(client.DownloadString("https://documents.i.opw.ie/alfresco/api/-default-/public/alfresco/versions/1/nodes/-my-/children"));                
             }
-            else if (listBox1.SelectedIndex == 1)
+            else if (listBox1.GetItemText(listBox1.SelectedItem).Equals("Shared"))
             {
                 url = "https://documents.i.opw.ie/share/page/context/shared/sharedfiles";
+                browseContent = serializer.DeserializeObject(client.DownloadString("https://documents.i.opw.ie/alfresco/api/-default-/public/alfresco/versions/1/nodes/-shared-/children"));
             }
             else
             {
-                url = "http://documents.i.opw.ie/share/page/site/" + shortNames[listBox1.SelectedIndex - 2];
+                //url = "http://documents.i.opw.ie/share/page/site/" + shortNames[listBox1.SelectedIndex - 2];
+
+                if (browseContent != null)
+                {
+                    foreach (dynamic d in browseContent["list"]["entries"])
+                    {
+                        if (d["entry"]["name"].Equals(listBox1.GetItemText(listBox1.SelectedItem)))
+                        {
+                            id = d["entry"]["id"];
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (dynamic d in sitesArray)
+                    {
+                        if (d["title"].Equals(listBox1.GetItemText(listBox1.SelectedItem)))
+                        {
+                            id = d["node"].Substring(d["node"].IndexOf("Store/") + 6);
+                        }
+                    }
+                }
+
+                browseContent = serializer.DeserializeObject(client.DownloadString(String.Format("https://documents.i.opw.ie/alfresco/api/-default-/public/alfresco/versions/1/nodes/{0}/children", id)));
+            }
+
+            listBox1.Items.Clear();
+
+            foreach (dynamic d in browseContent["list"]["entries"])
+            {
+                listBox1.Items.Add(d["entry"]["name"]);
             }
 
             //switch (selectedAction)
@@ -80,7 +104,7 @@ namespace SaveAttachments
             //        this.Close();
 
             //        break;
-                
+
             //    case "upload":
 
             //        MessageBox.Show("Uploading to Alfresco");
@@ -96,7 +120,31 @@ namespace SaveAttachments
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if (browseContent != null)
+            {
+                if (listBox1.GetItemText(listBox1.SelectedItem).Equals("Home"))
+                {
+                    button1.Text = "Open Home";
+                }
+                else if (listBox1.GetItemText(listBox1.SelectedItem).Equals("Shared"))
+                {
+                    button1.Text = "Open Shared";
+                }
+                else
+                {
+                    foreach (dynamic d in browseContent["list"]["entries"])
+                    {
+                        if (d["entry"]["name"].Equals(listBox1.GetItemText(listBox1.SelectedItem)) && d["entry"]["nodeType"].Equals("cm:folder"))
+                        {
+                            button1.Text = "Open folder";
+                        }
+                        else if (d["entry"]["name"].Equals(listBox1.GetItemText(listBox1.SelectedItem)) && d["entry"]["nodeType"].Equals("cm:content"))
+                        {
+                            button1.Text = "Select file";
+                        }
+                    }
+                }
+            }
         }
     }
 }
